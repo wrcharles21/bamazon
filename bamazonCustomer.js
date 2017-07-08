75b75748601f199f94e3c2c13e29ebe7cf0cb93e
William Charles <williamcharles@William-Charless-MacBook-Pro.local>
2017-07-08 12:59:04 -0700

    made one prompt and 2 query callback

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 var Table = require('cli-table');
 
+
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -9,13 +10,14 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+
 connection.connect(function(error,results){
 	connection.query("SELECT item_id, product_name, price FROM products" , function(error, results){
 		if (error) throw error;
 
 		
 		var table = new Table({
-			head: ["item id", "product name", "listed price"],
+			head: ["itemId", "product name", "listed Price"],
 
 		});
 
@@ -25,45 +27,50 @@ connection.connect(function(error,results){
 				);
 		}
 		console.log(table.toString());
-		itemSearch();
+		promptQuestions(results);
 		
 	})
 
-	function itemSearch() {
+	function promptQuestions(results) {
 	  inquirer
-	    .prompt({
-	      name: "item_id",
+	    .prompt([{
+	      name: "itemId",
 	      type: "input",
-	      message: "What is the Item ID that you are looking for?"
-	    })
-	    .then(function(answer){
-	    	console.log(answer.item_id);
-	    	connection.query("SELECT * FROM products WHERE ?",{item_id: answer.item_id},
-	    		function(error, results) {
-	    			console.log(results);
-	    		})
-	    	quantity();
-	    })
-	 }
-	 function quantity() {
-	 	inquirer
-	 	  .prompt({
-	 	  	name: "stock_quantity",
+	      message: "What is the Item ID that you are looking for?",
+	    }, {
+	    	name: "quantityInput",
 	 	  	type: "input",
 	 	  	message: "How many would you Like?"
-	 	})
-	 	.then(function(answer){
-	 		console.log(answer.stock_quantity);
-	 		connection.query("SELECT * FROM products WHERE id_input ? ",{stock_quantity: answer.stock_quantity},
-	 			function(error, results) {
-	 				if (stock_quantity<answer.stock_quantity){
-	 					console.log("insufficient Inventory!");
-	 				}else{
-	 					console.log("");
-	 					console.log(answer.stock_quantity + "Purchased");
-	 					console.log(results[0].product_name + "" + resluts[0].price);
-	 				}
-	 			})
-	 		})
-	 	}
-});
+	    }])
+	    .then(function(customerInput){
+	    	console.log(customerInput.itemId);
+	    	
+	    	connection.query("SELECT * FROM products WHERE item_id =?",[customerInput.itemId],
+	    		
+	    		function(error, results) {
+	    			
+	    			if (error){
+	    				console.log("item does not exist");
+	    			} 
+	    			console.log(results);
+	    			
+	    			
+	    			var totalPrice = results[0].price;
+	    			if (customerInput.quantityInput < results[0].stock_quantity){
+	    				connection.query("UPDATE products SET stock_quantity= stock_quantity - ? WHERE item_id = ?", [customerInput.quantityInput, customerInput.itemId], function(error, results) {
+	    					// if (error) throw error;
+	    					console.log("Order Competed. Your total is", totalPrice * customerInput.quantityInput);
+	    				})
+	    			} 
+	    			else {
+	    					console.log("Not enough in stock");
+
+	    					promptQuestions();
+
+	    				}
+	    			})
+	    		})
+	    	}
+
+});	
+	 
